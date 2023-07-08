8be963e9101c423653fba7219dde5578ccbd9bd9
Jack Lloyd <jack@randombit.net>
2023-07-08 08:23:31 -0400

    Avoid unwrap in the tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,132 +1,151 @@
 #[test]
-fn test_aead_parsing() {
+fn test_aead_parsing() -> Result<(), wycheproof::WycheproofError> {
     for test in wycheproof::aead::TestName::all() {
-        let _kat = wycheproof::aead::TestSet::load(test).unwrap();
+        let _kat = wycheproof::aead::TestSet::load(test)?;
     }
+    Ok(())
 }
 
 #[test]
-fn test_cipher_parsing() {
+fn test_cipher_parsing() -> Result<(), wycheproof::WycheproofError> {
     for test in wycheproof::cipher::TestName::all() {
-        let _kat = wycheproof::cipher::TestSet::load(test).unwrap();
+        let _kat = wycheproof::cipher::TestSet::load(test)?;
     }
+    Ok(())
 }
 
 #[test]
-fn test_dsa_parsing() {
+fn test_dsa_parsing() -> Result<(), wycheproof::WycheproofError> {
     for test in wycheproof::dsa::TestName::all() {
-        let _kat = wycheproof::dsa::TestSet::load(test).unwrap();
+        let _kat = wycheproof::dsa::TestSet::load(test)?;
     }
+    Ok(())
 }
 
 #[test]
-fn test_ecdh_parsing() {
+fn test_ecdh_parsing() -> Result<(), wycheproof::WycheproofError> {
     for test in wycheproof::ecdh::TestName::all() {
-        let _kat = wycheproof::ecdh::TestSet::load(test).unwrap();
+        let _kat = wycheproof::ecdh::TestSet::load(test)?;
     }
+    Ok(())
 }
 
 #[test]
-fn test_ecdsa_parsing() {
+fn test_ecdsa_parsing() -> Result<(), wycheproof::WycheproofError> {
     for test in wycheproof::ecdsa::TestName::all() {
-        let _kat = wycheproof::ecdsa::TestSet::load(test).unwrap();
+        let _kat = wycheproof::ecdsa::TestSet::load(test)?;
     }
+    Ok(())
 }
 
 #[test]
-fn test_eddsa_parsing() {
+fn test_eddsa_parsing() -> Result<(), wycheproof::WycheproofError> {
     for test in wycheproof::eddsa::TestName::all() {
-        let _kat = wycheproof::eddsa::TestSet::load(test).unwrap();
+        let _kat = wycheproof::eddsa::TestSet::load(test)?;
     }
+    Ok(())
 }
 
 #[test]
-fn test_ec_curve_parsing() {
+fn test_ec_curve_parsing() -> Result<(), wycheproof::WycheproofError> {
     for test in wycheproof::ec_curve::TestName::all() {
-        let _kat = wycheproof::ec_curve::TestSet::load(test).unwrap();
+        let _kat = wycheproof::ec_curve::TestSet::load(test)?;
     }
+    Ok(())
 }
 
 #[test]
-fn test_fpe_str_parsing() {
+fn test_fpe_str_parsing() -> Result<(), wycheproof::WycheproofError> {
     for test in wycheproof::fpe_str::TestName::all() {
-        let _kat = wycheproof::fpe_str::TestSet::load(test).unwrap();
+        let _kat = wycheproof::fpe_str::TestSet::load(test)?;
     }
+    Ok(())
 }
 
 #[test]
-fn test_fpe_list_parsing() {
+fn test_fpe_list_parsing() -> Result<(), wycheproof::WycheproofError> {
     for test in wycheproof::fpe_list::TestName::all() {
-        let _kat = wycheproof::fpe_list::TestSet::load(test).unwrap();
+        let _kat = wycheproof::fpe_list::TestSet::load(test)?;
     }
+    Ok(())
 }
 
 #[test]
-fn test_hkdf_parsing() {
+fn test_hkdf_parsing() -> Result<(), wycheproof::WycheproofError> {
     for test in wycheproof::hkdf::TestName::all() {
-        let _kat = wycheproof::hkdf::TestSet::load(test).unwrap();
+        let _kat = wycheproof::hkdf::TestSet::load(test)?;
     }
+    Ok(())
 }
 
 #[test]
-fn test_keywrap_parsing() {
+fn test_keywrap_parsing() -> Result<(), wycheproof::WycheproofError> {
     for test in wycheproof::keywrap::TestName::all() {
-        let _kat = wycheproof::keywrap::TestSet::load(test).unwrap();
+        let _kat = wycheproof::keywrap::TestSet::load(test)?;
     }
+    Ok(())
 }
 
 #[test]
-fn test_mac_parsing() {
+fn test_mac_parsing() -> Result<(), wycheproof::WycheproofError> {
     for test in wycheproof::mac::TestName::all() {
-        let _kat = wycheproof::mac::TestSet::load(test).unwrap();
+        let _kat = wycheproof::mac::TestSet::load(test)?;
     }
+    Ok(())
 }
 
 #[test]
-fn test_mac_with_nonce_parsing() {
+fn test_mac_with_nonce_parsing() -> Result<(), wycheproof::WycheproofError> {
     for test in wycheproof::mac_with_nonce::TestName::all() {
-        let _kat = wycheproof::mac_with_nonce::TestSet::load(test).unwrap();
+        let _kat = wycheproof::mac_with_nonce::TestSet::load(test)?;
     }
+    Ok(())
 }
 
 #[test]
-fn test_primality_parsing() {
+fn test_primality_parsing() -> Result<(), wycheproof::WycheproofError> {
     for test in wycheproof::primality::TestName::all() {
-        let _kat = wycheproof::primality::TestSet::load(test).unwrap();
+        let _kat = wycheproof::primality::TestSet::load(test)?;
     }
+    Ok(())
 }
 
 #[test]
-fn test_rsa_oaep_parsing() {
+fn test_rsa_oaep_parsing() -> Result<(), wycheproof::WycheproofError> {
     for test in wycheproof::rsa_oaep::TestName::all() {
-        let _kat = wycheproof::rsa_oaep::TestSet::load(test).unwrap();
+        let _kat = wycheproof::rsa_oaep::TestSet::load(test)?;
     }
+    Ok(())
 }
 
 #[test]
-fn test_rsa_pkcs1_decrypt_parsing() {
+fn test_rsa_pkcs1_decrypt_parsing() -> Result<(), wycheproof::WycheproofError> {
     for test in wycheproof::rsa_pkcs1_decrypt::TestName::all() {
-        let _kat = wycheproof::rsa_pkcs1_decrypt::TestSet::load(test).unwrap();
+        let _kat = wycheproof::rsa_pkcs1_decrypt::TestSet::load(test)?;
     }
+    Ok(())
 }
 
 #[test]
-fn test_rsa_pkcs1_verify_parsing() {
+fn test_rsa_pkcs1_verify_parsing() -> Result<(), wycheproof::WycheproofError> {
     for test in wycheproof::rsa_pkcs1_verify::TestName::all() {
-        let _kat = wycheproof::rsa_pkcs1_verify::TestSet::load(test).unwrap();
+        let _kat = wycheproof::rsa_pkcs1_verify::TestSet::load(test)?;
     }
+    Ok(())
 }
 
 #[test]
-fn test_rsa_pss_verify_parsing() {
+fn test_rsa_pss_verify_parsing() -> Result<(), wycheproof::WycheproofError> {
     for test in wycheproof::rsa_pss_verify::TestName::all() {
-        let _kat = wycheproof::rsa_pss_verify::TestSet::load(test).unwrap();
+        let _kat = wycheproof::rsa_pss_verify::TestSet::load(test)?;
     }
+    Ok(())
 }
 
 #[test]
-fn test_xdh_parsing() {
+fn test_xdh_parsing() -> Result<(), wycheproof::WycheproofError> {
     for test in wycheproof::xdh::TestName::all() {
-        let _kat = wycheproof::xdh::TestSet::load(test).unwrap();
+        let _kat = wycheproof::xdh::TestSet::load(test)?;
     }
+    Ok(())
 }
